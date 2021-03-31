e4788cb2e86170f8b357334ca9e3d4228d4f3279
haidarmy <haidar_my@apps.ipb.ac.id>
2021-04-01 03:17:57 +0700

    refactoring project structure & react-navigation installation

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+//import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Text>Halo worldo!</Text>
+      </ScrollView>
+    </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+        </Stack.Navigator>
+    )
+}
